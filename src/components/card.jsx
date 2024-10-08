97518c421a2b51ab257cb55dc0079e28c96e0070
Vineet Yadav <vineet.yadav@intimetec.com>
2024-10-08 13:59:43 +0530

    conditional rendenring in which i have taken button colors on the basis of its stock

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -6,16 +6,20 @@ function Card() {
       image:
         "https://images.unsplash.com/photo-1719937206098-236a481a2b6d?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       name: "fruits",
+      instock: true,
     },
     {
       image:
         "https://images.unsplash.com/photo-1726199029699-e314218e852d?q=80&w=1370&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       name: "Museum",
+
+      instock: false,
     },
     {
       image:
         "https://images.unsplash.com/photo-1615986201152-7686a4867f30?q=80&w=1450&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       name: "Books",
+      instock: true,
     },
   ];
 
@@ -38,6 +42,13 @@ function Card() {
                 suscipit illum debitis? Minima quod repellendus maiores
                 architecto!
               </p>
+              <button
+                className={`px-2 py-2 ${
+                  value.instock ? "bg-orange-300" : "bg-red-500"
+                } rounded-md mt-4 text-black `}
+              >
+                {value.instock ? "In Stock" : "Out of Stock"}
+              </button>
             </div>
           </div>
         ))}
